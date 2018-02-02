831784264bb1852b7a434e7372573d1c746075b4
唐向荣 <641418330@qq.com>
2018-02-02 09:59:27 +0800

    登陆页提交注册

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # wx-axf
 微信小程序版爱鲜蜂
+
+运行项目，需要先通过终端进入`db`目录，通过`npm install`安装依赖后键入`npm run db`启动`json-server`服务器，小程序才可以获取到数据
+
+微信开发者工具需要勾选不校验安全域名、`web-view域名`、`TLS`版本以及`HTTPS`证书
--- pages/login/login.js
+++ pages/login/login.js
@@ -1,3 +1,6 @@
+const app = getApp()
+const api = require('../../utils/api.js')
+
 // packageA/pages/login/login.js
 Page({
 
@@ -5,62 +8,76 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    // 控制提示条的显示隐藏
+    picHide: false,
+    phone: ''
   },
-
-  /**
-   * 生命周期函数--监听页面加载
+  /* 
+   * 手机号码输入框更改内容的时候同步data中的phone
+   * @param string value 输入的值
    */
-  onLoad: function (options) {
-  
+  changePhone (event) {
+    let phone = event.detail.value
+    this.setData({
+      phone: phone
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
+  /* 
+   * 手机号码输入框聚焦的时候隐藏图片
    */
-  onReady: function () {
-  
+  picHidefocus () {
+    this.setData({
+      picHide: true
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
+  /* 
+   * 手机号码输入框失焦的时候显示图片（延迟1秒钟）
    */
-  onShow: function () {
-  
+  picHideBlue () {
+    setTimeout(() => {
+      this.setData({
+        picHide: false
+      })
+    }, 1000)
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
+  /* 
+   * 点击确定按钮登陆
    */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  loginFn () {
+    let phone = this.data.phone
+    let reg = /^1[34578]\d{9}$/g
+    if (reg.test(phone)) {
+      // 验证通过
+      // 发请求验证在数据库是否存在
+      app.fetch(api.host + '/users?phone=' + phone)
+        .then(res => {
+          // 判断这个接口是否返回了数据，如果返回就是存在，没返回就是没有这个手机号
+          console.log(res)
+          if (res.length > 0) {
+            // 登陆成功
+            // 提取该用户的购物车数据
+            console.log('登陆成功')
+          } else {
+            // 数据库没有这个用户，注册
+            let userObj = {
+              // 手机号
+              phone: phone,
+              // 初始化所选地址
+              select_site: {}
+            }
+            wx.request({
+              url: 'http://course.io:3000/users/2',
+              method: 'DELETE',
+              success: res => {
+                console.log(res.data)
+              }
+            })
+            // app.fetch(api.host + '/users', "post", userObj)
+            //   .then(res => {
+            //     console.log(res)
+            //   })
+          }
+        })
+    }
   }
 })
