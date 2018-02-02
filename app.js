ceac38d5dbd98d08a34b0e0e74818a9eb5392cd7
唐向荣 <641418330@qq.com>
2018-02-02 17:46:56 +0800

    获取用户的购物车数据

--- app.js
+++ app.js
@@ -2,8 +2,35 @@
 let api = require('./utils/api.js')
 App({
   onLaunch () {
-    
+    // 读取保存在本地的用户信息
+    let userinfo = wx.getStorageSync('userinfo')
+    if (userinfo) {
+      // 如果userinfo不等于空,那么就提取里面的值
+      userinfo = JSON.parse(userinfo)
+      // 获取改用户的购物车数据
+      this.getCart(userinfo.id)
+        .then(res=>{
+          this.globalData.carts = res
+        })
+    }
   },
+  /* 
+   * 获取该用户对应的购物车数据
+   * @param   int   id    用户的id
+   * @return  arr   carts 购物车数据
+   */
+  getCart (id) {
+    return new Promise((resolve, reject) => {
+      this.fetch(api.host + '/carts?userId=' + id)
+        .then(res => {
+          resolve(res)
+        })
+    })
+  },
+  /* 
+   * 获取分类和商品数据
+   * @param callback cb 回调函数,用于在获取到商品数据后执行内容
+   */
   getComputedCategories (cb) {
     wx.showLoading({
       title: '加载中...',
@@ -30,10 +57,20 @@ App({
         wx.hideLoading()
       })
   },
+  /* 
+   * 全局的数据
+   */
   globalData: {
     // 保存的是合并后的商品数据
-    computedCategories: []
+    computedCategories: [],
+    // 购物车数据
+    carts: []
   },
+  /* 
+   * 封装的请求方法
+   * @param string method 请求的方法
+   * @param object data   请求携带的数据
+   */
   fetch(url, method = "get", data = {},) {
     return new Promise(function (resolve, reject) {
       
--- utils/api.js
+++ utils/api.js
@@ -1,4 +1,4 @@
 let api = {
-  host: 'http://172.20.10.2:3000'
+  host: 'http://127.0.0.1:3000'
 }
 module.exports = api
