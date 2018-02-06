4baead1cd59d4daaf28ad0c907f2c03a378feb6d
唐向荣 <641418330@qq.com>
2018-02-06 17:53:20 +0800

    添加地图

--- packageA/pages/select-site/select-site.js
+++ packageA/pages/select-site/select-site.js
@@ -1,16 +1,53 @@
+var bmap = require('../../../utils/bmap-wx.min.js')
+var wxMarkerData = [];
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    markers: [],
+    latitude: '',
+    longitude: '',
+    rgcData: {}
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+  makertap: function (e) {
+    var that = this;
+    var id = e.markerId;
+    that.showSearchInfo(wxMarkerData, id);
+  },
+  onLoad: function () {
+    var that = this;
+    var BMap = new bmap.BMapWX({
+      ak: 'VM1vQhGjq8KwvOPgI5WSM3NWPCaLNksa'
+    });
+    var fail = function (data) {
+      console.log(data)
+    };
+    var success = function (data) {
+      wxMarkerData = data.wxMarkerData;
+      that.setData({
+        markers: wxMarkerData
+      });
+      that.setData({
+        latitude: wxMarkerData[0].latitude
+      });
+      that.setData({
+        longitude: wxMarkerData[0].longitude
+      });
+    }
+    BMap.regeocoding({
+      fail: fail,
+      success: success,
+      iconPath: '/images/marker_red.png',
+      iconTapPath: '/images/marker_red.png'
+    });
+  },
+  showSearchInfo: function (data, i) {
+    var that = this;
+    that.setData({
+      rgcData: {
+        address: '地址：' + data[i].address + '\n',
+        desc: '描述：' + data[i].desc + '\n',
+        business: '商圈：' + data[i].business
+      }
+    });
   }
+
 })
