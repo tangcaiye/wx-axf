0269d79d865061d06764b744e5682b66e37fdb92
唐向荣 <641418330@qq.com>
2018-02-06 11:23:37 +0800

    完善详情页的添加减少商品数量

--- packageA/pages/product-item/product-item.js
+++ packageA/pages/product-item/product-item.js
@@ -1,6 +1,7 @@
 // packageA/pages/product-item/product-item.js
 let app = getApp()
 let api = require('../../../utils/api.js')
+let id = 0
 Page({
 
   /**
@@ -14,17 +15,24 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    let id = options.id
+    id = options.id
+  },
+  onShow () {
     // 获取全局的商品数据
+    this.getComputedCategories()
+  },
+  /* 
+   * 获取全局的商品数据
+   */
+  getComputedCategories () {
     let computedCategories = app.globalData.computedCategories
     if (computedCategories.length > 0) {// 如果已经获取到了
-      
+
       label:
       for (let i = 0; i < computedCategories.length; i++) {
         let products = computedCategories[i].products
         for (let j = 0; j < products.length; j++) {
-          if (products[j].id === id) {
-            console.log(products[j])
+          if (products[j].id === Number(id)) {
             this.setData({
               productInfo: products[j]
             })
@@ -41,5 +49,29 @@ Page({
           })
         })
     }
+  },
+  /* 
+   * 减少商品
+   */
+  subCart (event) {
+    let product = this.data.productInfo
+    // 追加product_id(商品id)属性
+    product.product_id = product.id
+    app.subCart(product)
+      .then(res => {
+        this.getComputedCategories()
+      })
+  },
+  /* 
+   * 添加商品
+   */
+  addCart (event) {
+    let product = this.data.productInfo
+    // 追加product_id(商品id)属性
+    product.product_id = product.id
+    app.addCart(product)
+      .then(res => {
+        this.getComputedCategories()
+      })
   }
 })
